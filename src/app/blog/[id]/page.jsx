2b02d65ea2e012952aa46a64d688311c8ac3607f
Erico Cesar <98761060+ericocesar@users.noreply.github.com>
2024-08-09 13:27:58 -0300

    retirariscoblog

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -62,11 +62,7 @@ const Blog = ({ params }) => {
             </div>
           </div>
         </div>
-        <img
-          src="/images/shape/shape_172.svg"
-          alt="shape"
-          className="lazy-img shapes shape-two"
-        />
+       
       </div>
 
       {/* Blog Section */}
